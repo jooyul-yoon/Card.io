a55d3e0816118a2d4479666f7c1326beab57b474
jooyul-yoon <juyeolyoon@gmail.com>
2022-10-14 14:59:49 +0000

    Signup page created

--- src/pages/Signup.tsx
+++ src/pages/Signup.tsx
@@ -1,7 +1,11 @@
 import React from 'react';
+import styled from 'styled-components';
 
+const Wrapper = styled.div`
+  margin-top: 60px;
+`;
 function Signup() {
-  return <div>sign up</div>;
+  return <Wrapper>sign up</Wrapper>;
 }
 
 export default React.memo(Signup);
