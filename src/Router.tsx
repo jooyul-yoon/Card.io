95b9ccb079f205c42f697008238031ddf6aa0454
jooyul-yoon <juyeolyoon@gmail.com>
2022-10-14 14:09:11 +0000

    221014 - Router Setting

---
+++ src/Router.tsx
@@ -0,0 +1,24 @@
+import DarkmodeBtn from 'components/DarkmodeBtn';
+import Navbar from 'components/Navbar';
+import Intro from 'pages/Intro';
+import Login from 'pages/Login';
+import Main from 'pages/Main';
+import Signup from 'pages/Signup';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+
+function Router() {
+  return (
+    <BrowserRouter>
+      <Navbar />
+      <Routes>
+        <Route path="/" element={<Intro />} />
+        <Route path="/signup" element={<Signup />} />
+        <Route path="/login" element={<Login />} />
+        <Route path="/team" element={<Main />} />
+      </Routes>
+      <DarkmodeBtn />
+    </BrowserRouter>
+  );
+}
+
+export default Router;
